ec2b064f4a15f88f5cb6b5e0d4f18705baa926c1
김민배 <manc9029@gmail.com>
2025-12-26 16:23:58 +0900

    header.jsp, list.jsp, productServlet.java 롤백

--- src/main/java/products/productsServlet.java
+++ src/main/java/products/productsServlet.java
@@ -1,34 +1,41 @@
 package products;
 
 import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/products/list")
+/**
+ * Servlet implementation class productsServlet
+ */
+@WebServlet("/productsServlet")
 public class productsServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        try {
-            productsDAO dao = productsDAO.getInstance();   // 또는 new productsDAO()
-            List<productsDTO> productList = dao.selectAllProducts();
-
-            request.setAttribute("productList", productList);
-            
-            request.getRequestDispatcher("/view/list.jsp").forward(request, response);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            request.setAttribute("errorMessage", e.getMessage());
-            request.getRequestDispatcher("/view/error.jsp").forward(request, response);
-        }
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public productsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
     }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
